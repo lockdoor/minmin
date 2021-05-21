056d8c4ef685522987c453f42eeb18bb2e17e4df
lockdoor <lockdoor@gmail.com>
2021-05-21 17:20:21 +0700

    main

--- connect-db.php
+++ connect-db.php
@@ -1,8 +1,12 @@
 <?php
-$servername = "localhost";
+/*$servername = "localhost";
 $username = "root";
 $password = "46671111";
-$dbname = "minmin";
+$dbname = "minmin";*/
+$servername = "localhost";
+$username = "namningx_root";
+$password = "c7Uq+y:6O9YKx7";
+$dbname = "namningx_minmin";
 $conn = new mysqli($servername, $username, $password, $dbname) or die ('Connection failed');
 $conn->set_charset('utf8');
 ?>
