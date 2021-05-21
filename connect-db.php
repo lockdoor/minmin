6e7422b0affa19d586b044333f2984f464e8a94b
lockdoor <lockdoor@gmail.com>
2021-05-21 19:47:57 +0700

    main

--- connect-db.php
+++ connect-db.php
@@ -7,7 +7,7 @@ $servername = "localhost";
 $username = "namningx_root";
 //$password = "c7Uq+y:6O9YKx7";
 $password = "{JDEhx@S(m@(";
-$dbname = "namningx_minmin";
+$dbname = "namningx_minmin_fake";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
