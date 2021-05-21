83f0e1b90a5a94421046a6c6e593125a0be55774
lockdoor <lockdoor@gmail.com>
2021-05-21 19:06:48 +0700

    main

--- connect-db.php
+++ connect-db.php
@@ -3,7 +3,7 @@
 $username = "root";
 $password = "46671111";
 $dbname = "minmin";*/
-$servername = "localhost";
+$servername = "localhost_";
 $username = "namningx_root";
 $password = "c7Uq+y:6O9YKx7";
 $dbname = "namningx_minmin";
